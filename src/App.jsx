910f056cd8dded8314f97eb296e830ea63bfb9b7
Ibrahim Hudson <you@example.com>
2025-02-20 00:27:48 -0500

    v.1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,11 @@ import { ShowFormButton } from "./buttons";
 // TODO: Add the gemini api so that it makes the template automatically
 // TODO: Create a different file for each component
 
-const Email = (props) => {
-  <main>
-    <h3>Subject: {props.subject}</h3>
-    <div id="body">
-      <p>{props.body}</p>
-    </div>
-  </main>;
-};
-
 function App() {
   return (
     <>
       <Header />
       <Intro />
-      <Email subject={undefined} body={undefined} />
       <ShowFormButton />
     </>
   );
